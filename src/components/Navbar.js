b4185408bcdb4b002bf82aafde550f097546907a
Gorrog <gourguesmaxime@gmail.com>
2022-12-13 16:19:31 +0100

    navbar setted, missing the responsive for mobile & missing the margin color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,21 +10,21 @@ export default function Navbar() {
               <p className="mr-10">contact@ponetteprod.com</p>
               <p>07 66 73 26 43</p>
             </div>
-            {/* <a className="" id="languages">
-              EN
-            </a> */}
           </div>
         </div>
         <hr></hr>
-        <div className="flex justify-between">
-          <div className="little-logo-color ">
-            <div className="little-logo my-10 ml-20">
-              <strong>Ponette</strong>
-              <br></br>
-              <strong>prod.</strong>
+        <div className="flex justify-between md: relative">
+          <div className="little-logo-color">
+            <div className="little-logo md: absolute md: left-20 md: top-5">
+              <div className="pr-3 pl-1">
+                <strong className="">Ponette</strong>
+              </div>
+              <div className="text-start pl-1">
+                <strong className="">prod.</strong>
+              </div>
             </div>
           </div>
-          <div className="flex justify-end items-baseline my-10 mr-10">
+          <div className="md:flex md:justify-end md:items-baseline md:my-10 mr-11 md: md:absolute md: right-0 md: top-0">
             <p className="mx-4">Accueil</p>
             <p className="mx-4">A propos</p>
             <p className="mx-4">Savoir-faire</p>
@@ -32,7 +32,7 @@ export default function Navbar() {
             <p className="mx-4">Nos client(e)s</p>
             <p className="mx-4">F.A.Q.</p>
             <p className="mx-4">Plus</p>
-            <button className="mx-4" id="devis">
+            <button className="mx-4 btn-primary" id="">
               Demander un devis
             </button>
           </div>
