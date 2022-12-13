bef26e5d2c57f1ce3086dfdfa38898d77dde62b7
Gorrog <gourguesmaxime@gmail.com>
2022-12-13 18:06:47 +0100

    created pages directory with home file, added Navbar component to the Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Navbar from "./components/Navbar.js";
+// import Navbar from "./components/Navbar.js";
+import Home from "./pages/home";
 
 function App() {
   return (
     <>
-    <Navbar />
+    {/* <Navbar /> */}
+    <Home />
     </>
   );
   // return (
